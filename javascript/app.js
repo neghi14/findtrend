90885f1c32ad4d43b14022ad57d1b693f5f244ab
neghi14 <sambasseynengimote@gmail.com>
2022-06-12 00:13:09 +0100

    added js and images with fully responsive hero section

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,17 @@
+let openNavbar = document.getElementById('open');
 
+let closeNavbar = document.getElementById('close');
+
+let navBarElement = document.querySelector('.mobile-nav');
+
+function open() {
+
+    navBarElement.classList.add('mobile-nav-open')
+}
+openNavbar.addEventListener('click', open);
+
+function close() {
+navBarElement.classList.remove('mobile-nav-open');;
+}
+
+closeNavbar.addEventListener('click', close);
